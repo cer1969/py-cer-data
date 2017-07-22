3454c30144324927828fbab25d1f4615628c26bd
Cristian Echeverr??a <cer1969@gmail.com>
2017-07-22 11:23:30 -0400

    Mejoras

--- ox/ox.py
+++ ox/ox.py
@@ -6,8 +6,7 @@ from munch import Munch
 #---------------------------------------------------------------------------------------------------
 
 class AttrDict(dict):
-    __slots__ = ()
-    #__getattr__ = dict.__getitem__
+    __slots__   = ()
     __setattr__ = dict.__setitem__
 
     def __getattr__(self, name):
@@ -23,30 +22,9 @@ class Book(object):
 
     def __init__(self, filepath, read_only=True, data_only=True):
         self._wb = ox.load_workbook(filepath, read_only=read_only, data_only=data_only)
-    
-    def getSheet(self, name):
+
+    def get_sheet(self, name):
         return Sheet(self, name)
-    
-    def getData(self, shname, rows, cols, dict_class=AttrDict):
-        sh = self._wb[shname]
-        headers = [sh.cell(row=rows[0], column=x).value for x in range(*cols)]
-        data = []
-        for i in range(rows[0] + 1, rows[1]):
-            row = [sh.cell(row=i, column=x).value for x in range(*cols)]
-            data.append(dict_class(zip(headers, row)))
-        return data
-    
-    def getRowIterator(self, sheet, rows=(1, None), cols=(1, None)):
-        sh = self._wb[sheet]
-        return sh.iter_rows(min_row=rows[0], max_row=rows[1], min_col=cols[0], max_col=cols[1])
-    
-    def getMuchData(self, sheet, rows=(1, None), cols=(1, None)):
-        it = self.getRowIterator(sheet, rows, cols)
-        headers = [c.value for c in next(it)]
-        data = []
-        for r in it:
-            data.append(Munch(zip(headers, [c.value for c in r])))
-        return data
 
 
 #---------------------------------------------------------------------------------------------------
@@ -56,23 +34,13 @@ class Sheet(object):
     def __init__(self, book, name):
         self._sh = book._wb[name]
 
-    def getData(self, rows, cols, dict_class=AttrDict):
-        sh = self._sh
-        headers = [sh.cell(row=rows[0], column=x).value for x in range(*cols)]
-        data = []
-        for i in range(rows[0] + 1, rows[1]):
-            row = [sh.cell(row=i, column=x).value for x in range(*cols)]
-            data.append(dict_class(zip(headers, row)))
-        return data
-    
-    def getRowIterator(self, rows=(1, None), cols=(1, None)):
-        sh = self._sh
-        return sh.iter_rows(min_row=rows[0], max_row=rows[1], min_col=cols[0], max_col=cols[1])
-    
-    def getMuchData(self, rows=(1, None), cols=(1, None)):
-        it = self.getRowIterator(rows, cols)
+    def iter_data(self, rows=(1, None), cols=(1, None)):
+        return self._sh.iter_rows(min_row=rows[0], max_row=rows[1], min_col=cols[0], max_col=cols[1])
+
+    def get_data(self, rows, cols, dict_class=Munch):
+        it = self.iter_data(rows, cols)
         headers = [c.value for c in next(it)]
         data = []
         for r in it:
-            data.append(Munch(zip(headers, [c.value for c in r])))
+            data.append(dict_class(zip(headers, [c.value for c in r])))
         return data

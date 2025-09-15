cab83b54f453b541bdf72f2813deb7b3dea3f808
cer1969 <cer1969@gmail.com>
2025-09-15 12:40:03 -0300

    Se agregan métodos para leer de tablas con nombre

--- opx/opx.py
+++ opx/opx.py
@@ -13,6 +13,23 @@ class Book(object):
 
     def get_sheet(self, name):
         return Sheet(self, name)
+    
+    def get_table_data(self, sheet: str, table: str, dict_class=dict):
+        """
+        Returns a list of dictionaries containing the data from the specified table in 
+        the specified sheet.
+        The workbook must be opened with read_only=False.
+
+        Parameters:
+        sheet (str): The name of the sheet to read from.
+        table (str): The name of the table to read from.
+        dict_class (type): The type of dictionary to use for the result (default is dict).
+
+        Returns:
+        list of dict_class: A list of dictionaries containing the data from the specified 
+        table in the specified sheet.
+        """
+        return Sheet(self, sheet).get_table_data(table, dict_class)
 
 
 #---------------------------------------------------------------------------------------------------
@@ -41,3 +58,13 @@ class Sheet(object):
         for r in it:
             data.append(dict_class(zip(headers, [c.value for c in r])))
         return data
+    
+    def get_table_data(self, table, dict_class=dict):
+        # REQUIERE read_only=False
+        t = self._sh.tables[table]
+        data = self._sh[t.ref]
+
+        filas = [[celda.value for celda in fila] for fila in data]
+        headers = filas[0]
+
+        return [dict_class(zip(headers, fila)) for fila in filas[1:]]
